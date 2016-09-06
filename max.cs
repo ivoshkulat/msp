81d3876728b5934052c87e36666eacab691e4957
ivoshkulat <igor.voshkulat@gmail.com>
2016-09-06 21:50:18 +0300

    Max v.3

--- max.cs
+++ max.cs
@@ -1,18 +1,33 @@
 class Max : IOperation
     {
+        private readonly int my_opp_id;
+        private float status;
+        public Max()
+        {
+            status = 0;
+            my_opp_id = 9;
+        }
+
         public double result
         {
             get
             {
-                return 0;
+                data_validation();
+                return max(variable_1, variable_2);
             }
-            set {  }
+            set { result_validation_set(); }
         }
-        public double variable_1 { get {return 0}; set; }
-        public double variable_2 { get {return 0}; set; }
+        public double variable_1 { get; set; }
+        public double variable_2 { get; set; }
 
-        public int opp_id =>return 0;
-        public float Status => return 0;
+        public int opp_id => my_opp_id;
+        public float Status => status;
+        private double max(double a, double b)
+        {
+            status = 1;
+            return (a > b) ? a : b;
+        }
+
         private void data_validation()
         {
             if (double.IsNaN(variable_1))
@@ -27,8 +42,8 @@ class Max : IOperation
             }
             status = 0.5F;
         }
-        private double max(double a, double b)
+        private void result_validation_set()
         {
-            return 1;
+            throw new Exception("FIG VAM!!!");
         }
     }
